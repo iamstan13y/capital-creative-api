859837f346132330fc20da6c943e3926b76f2a4e
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 20:41:21 +0200

    Category CRUD Config.

---
+++ CapitalCreative.API/Models/Local/CategoryRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CapitalCreative.API.Models.Local
+{
+    public class CategoryRequest
+    {
+        public string? Name { get; set; }
+    }
+
+    public class UpdateCategoryRequest : CategoryRequest
+    {
+        public int Id { get; set; }
+    }
+}
