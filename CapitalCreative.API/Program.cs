9571b33ba792fbfa32433c6e436d95df4bf6d9cb
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 16:43:59 +0200

    24d8c4de - II.

--- CapitalCreative.API/Program.cs
+++ CapitalCreative.API/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
