e57cccc23f1d69a59a57944f1783e56465214d0b
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 19:54:14 +0200

    Unit of Work Config & DI.

--- CapitalCreative.API/Program.cs
+++ CapitalCreative.API/Program.cs
@@ -1,4 +1,6 @@
 using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+using CapitalCreative.API.Models.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddControllers();
 
