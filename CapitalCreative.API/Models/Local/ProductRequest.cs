75cc7fac7771c2201520a3dc3d8c79bf428f5f8b
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 22:32:11 +0200

    Add Product Implementation.

---
+++ CapitalCreative.API/Models/Local/ProductRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CapitalCreative.API.Models.Local
+{
+    public class ProductRequest
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+    }
+}
