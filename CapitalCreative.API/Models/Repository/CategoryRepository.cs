bf59131c24b39fd93a80061557ddf3f17f020dce
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 20:32:18 +0200

    Category Repository UoW Config.

---
+++ CapitalCreative.API/Models/Repository/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using CapitalCreative.API.Models.Data;
+using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+
+namespace CapitalCreative.API.Models.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
