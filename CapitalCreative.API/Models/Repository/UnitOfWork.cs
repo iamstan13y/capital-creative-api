a107912454773d569745b9214013c6eea99e1666
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 19:54:11 +0200

    Unit of Work Config & DI.

---
+++ CapitalCreative.API/Models/Repository/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+
+namespace CapitalCreative.API.Models.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public ISubscriptionRepository Subscription { get; private set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            Role = new RoleRepository(context);
+            _context = context;
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+    }
+}
