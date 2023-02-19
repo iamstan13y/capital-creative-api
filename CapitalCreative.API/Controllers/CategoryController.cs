7456d30ddd95c45a9523983a6a94a2225c0e0be8
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 20:41:17 +0200

    Category CRUD Config.

---
+++ CapitalCreative.API/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using CapitalCreative.API.Models.Data;
+using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapitalCreative.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _CategoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository) => _CategoryRepository = categoryRepository;
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CategoryRequest request)
+        {
+            var result = await _CategoryRepository.AddAsync(new Category
+            {
+                Name = request.Name
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _CategoryRepository.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _CategoryRepository.FindAsync(id);
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCategoryRequest request)
+        {
+            var result = await _CategoryRepository.UpdateAsync(new Category
+            {
+                Id = request.Id,
+                Name = request.Name
+            });
+
+            return Ok(result);
+        }
+    }
+}
