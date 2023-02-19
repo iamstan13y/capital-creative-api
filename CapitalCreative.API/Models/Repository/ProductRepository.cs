a500f57923668a4ce2ff5a9d6e64de96c75cc500
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 22:26:14 +0200

    Product Repository & UoW Config.

---
+++ CapitalCreative.API/Models/Repository/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CapitalCreative.API.Models.Data;
+using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+
+namespace CapitalCreative.API.Models.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
