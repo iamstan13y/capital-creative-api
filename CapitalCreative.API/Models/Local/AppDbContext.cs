0dc45520c0eca32982638def4d1d80354feb9685
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 16:43:52 +0200

    24d8c4de - II.

---
+++ CapitalCreative.API/Models/Local/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CapitalCreative.API.Models.Local
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
