35fbca428ec2978885fe35039e6391a71e3f1878
Keith Stanley <iamstan13y@gmail.com>
2023-02-19 20:23:08 +0200

    Category Model & DB Migrations.

--- CapitalCreative.API/Models/Local/AppDbContext.cs
+++ CapitalCreative.API/Models/Local/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CapitalCreative.API.Models.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CapitalCreative.API.Models.Local
 {
@@ -7,5 +8,7 @@ namespace CapitalCreative.API.Models.Local
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
