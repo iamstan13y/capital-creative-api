550b9532debcdfbae05bc8bc39b35154a90f0d2a
Keith Stanley <iamstan13y@gmail.com>
2023-02-20 23:32:41 +0200

    Project Model & DB Migrations.

---
+++ CapitalCreative.API/Models/Data/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace CapitalCreative.API.Models.Data
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
