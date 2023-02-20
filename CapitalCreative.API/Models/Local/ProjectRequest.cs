f18e28cddd1c86e87eae7db9531898a6b17525b1
Keith Stanley <iamstan13y@gmail.com>
2023-02-20 23:43:39 +0200

    Add Project Implementation.

---
+++ CapitalCreative.API/Models/Local/ProjectRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CapitalCreative.API.Models.Local
+{
+    public class ProjectRequest
+    {
+        public string? Title { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
