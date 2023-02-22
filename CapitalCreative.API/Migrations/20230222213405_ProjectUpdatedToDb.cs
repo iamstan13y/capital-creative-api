65eb9480f252b659854bcfdf1cda2b4793c68912
Keith Stanley <iamstan13y@gmail.com>
2023-02-22 23:39:31 +0200

    Project Model DB Migrations.

---
+++ CapitalCreative.API/Migrations/20230222213405_ProjectUpdatedToDb.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapitalCreative.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectUpdatedToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Project",
+                newName: "ShortDescription");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Project",
+                newName: "DateCommissioned");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DetailedDescription",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DetailedDescription",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Project");
+
+            migrationBuilder.RenameColumn(
+                name: "ShortDescription",
+                table: "Project",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCommissioned",
+                table: "Project",
+                newName: "Date");
+        }
+    }
+}
