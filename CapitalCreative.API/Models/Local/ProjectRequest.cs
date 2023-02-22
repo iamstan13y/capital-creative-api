08103ccfa4e9a76d379e1318d34bdcc9b8abfaa2
Keith Stanley <iamstan13y@gmail.com>
2023-02-22 23:33:27 +0200

    Project Model Updated Prperties.

--- CapitalCreative.API/Models/Local/ProjectRequest.cs
+++ CapitalCreative.API/Models/Local/ProjectRequest.cs
@@ -4,8 +4,10 @@
     {
         public string? Title { get; set; }
         public string? Location { get; set; }
-        public string? Description { get; set; }
-        public DateTime Date { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? DetailedDescription { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime DateCommissioned { get; set; }
     }
 
     public class UpdateProjectRequest : ProjectRequest
