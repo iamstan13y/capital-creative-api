577e0b684e50ed1d0b11412de0f2c2b6bd7c9739
Keith Stanley <iamstan13y@gmail.com>
2023-02-24 12:53:34 +0200

    Contact Message Implementation

---
+++ CapitalCreative.API/Models/Local/EmailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CapitalCreative.API.Models.Local
+{
+    public class EmailRequest
+    {
+        public string? To { get; set; }
+        public string? From { get; set; }
+        public string? Name { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
