76761f42a7baee5fccf1efe4566f6614697b8df8
Keith Stanley <iamstan13y@gmail.com>
2023-02-24 12:53:25 +0200

    Contact Message Implementation

--- CapitalCreative.API/Controllers/ContactController.cs
+++ CapitalCreative.API/Controllers/ContactController.cs
@@ -1,4 +1,6 @@
-﻿using CapitalCreative.API.Models.Repository.IRepository;
+﻿using CapitalCreative.API.Models.Data;
+using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CapitalCreative.API.Controllers
@@ -22,6 +24,23 @@ namespace CapitalCreative.API.Controllers
         {
             var result = await _unitOfWork.Contact.FindAsync(id);
             if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ContactRequest request)
+        {
+            var result = await _unitOfWork.Contact.AddAsync(new Contact
+            {
+                Email = request.Email,
+                FullName = request.FullName,
+                Message = request.Message,
+                PhoneNumber = request.PhoneNumber,
+                Subject = request.Subject
+            });
+
+            if (!result.Success) return BadRequest(result);
 
             return Ok(result);
         }
