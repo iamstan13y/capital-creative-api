c51b0591ebdbd81d322be7f605d169266ba97412
Keith Stanley <iamstan13y@gmail.com>
2023-02-24 12:53:50 +0200

    Contact Message Implementation

---
+++ CapitalCreative.API/Services/IServices/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using CapitalCreative.API.Models.Local;
+
+namespace CapitalCreative.API.Services.IServices
+{
+    public interface IEmailService
+    {
+        Task<Result<string>> SendEmailAsync(EmailRequest email);
+    }
+}
