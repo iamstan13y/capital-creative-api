11092a73058a9664575f097db20a4c91fe14ebcf
Keith Stanley <iamstan13y@gmail.com>
2023-02-24 10:31:58 +0200

    Contact Repository Pattern & UoW DI.

--- CapitalCreative.API/Models/Repository/UnitOfWork.cs
+++ CapitalCreative.API/Models/Repository/UnitOfWork.cs
@@ -10,12 +10,14 @@ namespace CapitalCreative.API.Models.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public IProjectRepository Project { get; private set; }
+        public IContactRepository Contact { get; private set; }
 
         public UnitOfWork(AppDbContext context)
         {
             Category = new CategoryRepository(context);
             Product = new ProductRepository(context);
             Project = new ProjectRepository(context);
+            Contact = new ContactRepository(context);
             _context = context;
         }
 
