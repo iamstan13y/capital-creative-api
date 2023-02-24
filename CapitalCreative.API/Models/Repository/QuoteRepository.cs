c5e34ddcc0b31731d29027cfbf356e1d4e82ca25
Keith Stanley <iamstan13y@gmail.com>
2023-02-25 00:04:43 +0200

    Quote Repository Pattern & UoW DI.

---
+++ CapitalCreative.API/Models/Repository/QuoteRepository.cs
@@ -0,0 +1,13 @@
+﻿using CapitalCreative.API.Models.Data;
+using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+
+namespace CapitalCreative.API.Models.Repository
+{
+    public class QuoteRepository : Repository<Quote>, IQuoteRepository
+    {
+        public QuoteRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
