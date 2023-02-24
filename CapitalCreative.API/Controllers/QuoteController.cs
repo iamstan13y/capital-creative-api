b6c2b39fdbd9bee35b80d3bdafacf52e82c11b44
Keith Stanley <iamstan13y@gmail.com>
2023-02-25 00:09:52 +0200

    Added Quote Controller.

---
+++ CapitalCreative.API/Controllers/QuoteController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapitalCreative.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class QuoteController : ControllerBase
+    {
+    }
+}
