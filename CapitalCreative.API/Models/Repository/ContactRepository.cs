97e558d1cf59d64dc95c2d39b19abd41d13f6441
Keith Stanley <iamstan13y@gmail.com>
2023-02-24 10:31:54 +0200

    Contact Repository Pattern & UoW DI.

---
+++ CapitalCreative.API/Models/Repository/ContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using CapitalCreative.API.Models.Data;
+using CapitalCreative.API.Models.Local;
+using CapitalCreative.API.Models.Repository.IRepository;
+
+namespace CapitalCreative.API.Models.Repository
+{
+    public class ContactRepository : Repository<Contact>, IContactRepository
+    {
+        public ContactRepository(AppDbContext context) : base(context)
+        {
+        }     
+    }
+}
