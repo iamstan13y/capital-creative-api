72aba061698b22574d47a7020c96dfa5f3cd8408
Keith Stanley <iamstan13y@gmail.com>
2023-02-24 23:59:51 +0200

    Quote Model & DB Migrations.

---
+++ CapitalCreative.API/Migrations/20230224215912_QuoteToDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapitalCreative.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuoteToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Appliances = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Budget = table.Column<double>(type: "float", nullable: false),
+                    ChoosingTheRightSystem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HowDidYouHearAboutUs = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OtherInformation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoofPitchedOrFlat = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoofType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SystemRequirements = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+        }
+    }
+}
