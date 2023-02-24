4cd52f8babf3a1a06c6f86adb68102c0fc0cdf4c
Keith Stanley <iamstan13y@gmail.com>
2023-02-25 00:00:03 +0200

    Quote Model & DB Migrations.

---
+++ CapitalCreative.API/Models/Data/Quote.cs
@@ -0,0 +1,19 @@
+﻿namespace CapitalCreative.API.Models.Data
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string? Address { get; set; }
+        public string? Appliances { get; set; }
+        public double Budget { get; set; }
+        public string? ChoosingTheRightSystem { get; set; }
+        public string? Email { get; set; }
+        public string? FullName { get; set; }
+        public string? HowDidYouHearAboutUs { get; set; }
+        public string? OtherInformation { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? RoofPitchedOrFlat { get; set; }
+        public string? RoofType { get; set; }
+        public string? SystemRequirements { get; set; }
+    }
+}
