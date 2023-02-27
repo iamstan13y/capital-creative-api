b21139ed68a1aac433a67ea1199d96fed4d5172c
Keith Stanley <iamstan13y@gmail.com>
2023-02-27 02:47:47 +0200

    Product HTTP POST & PUT Updates.

--- CapitalCreative.API/Models/Local/ProductRequest.cs
+++ CapitalCreative.API/Models/Local/ProductRequest.cs
@@ -5,6 +5,7 @@
         public int CategoryId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
         public double Price { get; set; }
     }
 
